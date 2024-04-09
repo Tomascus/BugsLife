86917169ee8ae82c55686f50069793962c803923
Tomas Szabo <bady.tomas33@gmail.com>
2024-04-09 22:44:43 +0100

    Initial App Setup

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace sf;
+using namespace std;
+
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Bugs Life, The Game!");
+
+    return 0;
+
+
+}
